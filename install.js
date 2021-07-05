67ec01fe531bba702cd1ef9e661690ffbf37b3e2
Mitchell R <support@bettercorp.co.za>
2021-07-06 00:59:06 +0200

    Install scripts now set scripts etc...

--- install.js
+++ install.js
@@ -13,70 +13,10 @@ if (CWD.indexOf('@bettercorp') >= 0) {
 
 console.log(`INSTALL SCRIPT FOR @bettercorp/service-base in ${CWD}`);
 
-const packaggeJSONFile = PATH.join(CWD, './package.json');
-if (FS.existsSync(packaggeJSONFile))
-  if (JSON.parse(FS.readFileSync(packaggeJSONFile).toString()).name.indexOf('@bettercorp/service-base') === 0)
-    return console.log('Self install. ignoring install script.');
-
-
-const configFile = PATH.join(CWD, './sec.config.json');
-if (!FS.existsSync(configFile)) {
-  console.log(`Creating config file... (${configFile})`);
-  FS.writeFileSync(configFile, '{"enabledPlugins": {}, "plugins": {}, "mappedPlugins": {}}');
-}
-
-const srcDir = PATH.join(CWD, './src');
+const srcDir = PATH.join(CWD, `./src`);
 if (!FS.existsSync(srcDir)) {
-  console.log(`Creating SRC dir... (${srcDir})`);
+  console.log(`Creating src dir... (${srcDir})`);
   FS.mkdirSync(srcDir);
-}
-
-const isTS = FS.existsSync(PATH.join(CWD, './tsconfig.json'));
-
-let readPackageJsonFile = JSON.parse(FS.readFileSync(packaggeJSONFile).toString())
-if (Tools.isNullOrUndefined(readPackageJsonFile.scripts)) {
-  readPackageJsonFile.scripts = {};
-}
-const scripts = {
-  dev: "nodemon -L --watch src/**/*.ts --watch plugins/**/*.ts --watch sec.config.json --exec ts-node src/index.ts",
-  start: "node lib/index.js",
-  build: isTS ? "tsc" : undefined,
-  publish: "npm publish",
-  version: "node ./node_modules/@bettercorp/service-base/build/version.js $0"
-}
-if (Tools.isNullOrUndefined(readPackageJsonFile.scripts)) {
-  readPackageJsonFile.scripts = {};
-}
-let pakUpdates = false;
-for (let key of Object.keys(scripts)) {
-  if (Tools.isNullOrUndefined(scripts[key])) continue;
-  
-  if (readPackageJsonFile.scripts[key] !== scripts[key]) {
-    readPackageJsonFile.scripts[key] = scripts[key];
-    pakUpdates = true;
-  }
-}
-if (Tools.isNullOrUndefined(readPackageJsonFile.files)) {
-  readPackageJsonFile.files = ["lib/**/*"];
-  pakUpdates = true;
-}
-if (pakUpdates) {
-  console.log(`Updating package scripts for you... (${packaggeJSONFile})`);
-  FS.writeFileSync(packaggeJSONFile, JSON.stringify(readPackageJsonFile))
-}
-
-const srcIndex = PATH.join(CWD, `./src/index.${isTS ? 'ts' : 'js'}`);
-if (!FS.existsSync(srcIndex)) {
-  console.log(`Creating Main index file... (${srcIndex})`);
-  FS.writeFileSync(srcIndex, isTS ?
-    "import ServiceBase from '@bettercorp/service-base';\n\n" +
-    "const SB = new ServiceBase();\n" +
-    "SB.init();\n" +
-    "SB.run();" :
-    'const ServiceBase = require("@bettercorp/service-base");\n\n' +
-    "const SB = new (ServiceBase.default || ServiceBase)();\n" +
-    "SB.init();\n" +
-    "SB.run();");
 }
 
 const pluginsDir = PATH.join(CWD, `./src/plugins`);
@@ -95,6 +35,81 @@ const dockerFile = PATH.join(dockerDir, `./DockerFile`);
 if (!FS.existsSync(dockerFile) && FS.existsSync(dockerSrcFile)) {
   console.log(`Creating docker build file... (${dockerSrcFile} -> ${dockerFile})`);
   FS.copyFileSync(dockerSrcFile, dockerFile)
+}
+
+const isTS = FS.existsSync(PATH.join(CWD, './tsconfig.json'));
+
+let readPackageJsonFile = JSON.parse(FS.readFileSync(packaggeJSONFile).toString())
+if (Tools.isNullOrUndefined(readPackageJsonFile.scripts)) {
+  readPackageJsonFile.scripts = {};
+}
+const appScripts = {
+  dev: "nodemon -L --watch src/**/*.ts --watch plugins/**/*.ts --watch sec.config.json --exec ts-node src/index.ts",
+  start: "node lib/index.js",
+  build: isTS ? "tsc" : undefined,
+  publish: "npm publish",
+  version: "node ./node_modules/@bettercorp/service-base/build/version.js $0"
+}
+const libScripts = {
+  build: isTS ? "tsc" : undefined,
+  publish: "npm publish",
+  version: "node ./node_modules/@bettercorp/service-base/build/version.js $0"
+}
+
+let libInstall = false;
+
+const packaggeJSONFile = PATH.join(CWD, './package.json');
+if (FS.existsSync(packaggeJSONFile))
+  if (JSON.parse(FS.readFileSync(packaggeJSONFile).toString()).name.indexOf('@bettercorp/service-base') === 0)
+    libInstall = true;
+
+let scripts = libScripts;
+if (!libInstall) {
+  scripts = appScripts;
+}
+if (Tools.isNullOrUndefined(readPackageJsonFile.scripts)) {
+  readPackageJsonFile.scripts = {};
+}
+let pakUpdates = false;
+for (let key of Object.keys(scripts)) {
+  if (Tools.isNullOrUndefined(scripts[key])) continue;
+
+  if (readPackageJsonFile.scripts[key] !== scripts[key]) {
+    readPackageJsonFile.scripts[key] = scripts[key];
+    pakUpdates = true;
+  }
+}
+if (Tools.isNullOrUndefined(readPackageJsonFile.files)) {
+  readPackageJsonFile.files = ["lib/**/*"];
+  pakUpdates = true;
+}
+if (pakUpdates) {
+  console.log(`Updating package scripts for you... (${packaggeJSONFile})`);
+  FS.writeFileSync(packaggeJSONFile, JSON.stringify(readPackageJsonFile))
+}
+
+if (libInstall) {
+  return console.log('Self install. ignoring app install script.');
+}
+
+const configFile = PATH.join(CWD, './sec.config.json');
+if (!FS.existsSync(configFile)) {
+  console.log(`Creating config file... (${configFile})`);
+  FS.writeFileSync(configFile, '{"enabledPlugins": {}, "plugins": {}, "mappedPlugins": {}}');
+}
+
+const srcIndex = PATH.join(CWD, `./src/index.${isTS ? 'ts' : 'js'}`);
+if (!FS.existsSync(srcIndex)) {
+  console.log(`Creating Main index file... (${srcIndex})`);
+  FS.writeFileSync(srcIndex, isTS ?
+    "import ServiceBase from '@bettercorp/service-base';\n\n" +
+    "const SB = new ServiceBase();\n" +
+    "SB.init();\n" +
+    "SB.run();" :
+    'const ServiceBase = require("@bettercorp/service-base");\n\n' +
+    "const SB = new (ServiceBase.default || ServiceBase)();\n" +
+    "SB.init();\n" +
+    "SB.run();");
 }
 
 console.log('INSTALL COMPLETE FOR @bettercorp/service-base');
