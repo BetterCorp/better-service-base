2adc16a6c6d8579e95a23c54cd6e54c7807cd391
Mitchell R <support@bettercorp.co.za>
2021-07-09 01:13:05 +0200

    Updating versioning

--- build/version.js
+++ build/version.js
@@ -7,6 +7,7 @@ let version = packageJSON.version;
 let buildTag = '';
 let outputOnly = false;
 let outputPackageName = false;
+let BSBOutput = false;
 for (let arg of args) {
   //console.log(`FARG: ${arg}`)
   if (arg.indexOf('--version=') >= 0) {
@@ -22,6 +23,9 @@ for (let arg of args) {
   }
   if (arg.indexOf('--package') >= 0) {
     outputPackageName = true;
+  }
+  if (arg.indexOf('--bsb') >= 0) {
+    BSBOutput = true;
   }
 }
 let versionSplit = version.split('-');
@@ -53,7 +57,15 @@ if (seconds.length == 1)
   seconds = `0${seconds}`
 let micro = `${now.getFullYear()}${month}${day}${hour}${minutes}${seconds}${tag}`;
 packageJSON.version = `${major}.${minor}.${micro}`;
-if (outputPackageName) {
+if (BSBOutput) {
+  if (!fs.existsSync('./_exports'))
+    fs.writeFileSync('./_exports')
+  fs.writeFileSync('./exports/PACKAGE_VERSION', packageJSON.version);
+  fs.writeFileSync('./exports/PACKAGE_NAME', packageJSON.name.replace('@bettercorp/service-base-', ''));
+  if (packageJSON.name.indexOf('@bettercorp/service-base-') >= 0) {
+    fs.writeFileSync('./exports/RUN_DOCKER', 'true');
+  }
+} else if (outputPackageName) {
   if (packageJSON.name.indexOf('@bettercorp/service-base-') < 0) {
     console.log('false')
   } else {
