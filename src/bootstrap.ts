182f7cf18aa48e9bb24113bbfd400eec5666f934
Mitchell R <support@bettercorp.co.za>
2021-08-27 16:36:50 +0200

    adding events/log to plugin name

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -26,20 +26,32 @@ const types: ReadonlyArray<PluginTypes> = ['plugin', 'logger', 'events'];
     .alias('help', 'h')
     .argv;
 
-  const dstPluginDir = path.join(cwd(), 'src/plugins', argv.name);
+  let pluginName = argv.name;
+  if (argv.type === 'events' && pluginName.indexOf('events-') !== 0) {
+    pluginName = `events-${ pluginName }`;
+  } else if (argv.type === 'logger' && pluginName.indexOf('log-') !== 0) {
+    pluginName = `log-${ pluginName }`;
+  } else if (argv.type === 'plugin') {
+    if (pluginName.indexOf('events-') === 0)
+      pluginName = pluginName.substring(7);
+    if (pluginName.indexOf('log-') === 0)
+      pluginName = pluginName.substring(4);
+  }
+
+  const dstPluginDir = path.join(cwd(), 'src/plugins', pluginName);
 
   if (fs.existsSync(dstPluginDir)) {
-    throw `Plugin ${ argv.name } already exists!`;
+    throw `Plugin ${ pluginName } already exists!`;
   }
 
   let srcCode: string = fs.readFileSync(path.join(SBBaseDir, 'templates', `${ argv.type }.ts`)).toString();
-  srcCode = srcCode.replace(/demo/g, argv.name);
+  srcCode = srcCode.replace(/demo/g, pluginName);
 
   fs.mkdirSync(dstPluginDir);
   fs.writeFileSync(path.join(dstPluginDir, 'plugin.ts'), srcCode);
   fs.copyFileSync(path.join(SBBaseDir, 'templates', 'sec.config.ts'), path.join(dstPluginDir, 'sec.config.ts'));
 
-  console.log(`New plugin created ${ argv.name } of type ${ argv.type }`);
+  console.log(`New plugin created ${ pluginName } of type ${ argv.type }`);
 
   const installer = path.join(SBBaseDir, "./lib/ServiceBase.js");
   console.log("INSTALL FINAL : AUTOLOAD: " + installer);
@@ -47,6 +59,6 @@ const types: ReadonlyArray<PluginTypes> = ['plugin', 'logger', 'events'];
   const SB = new ServiceBase.default(cwd());
   SB.config().then(() => console.log("INSTALL COMPLETE FOR @bettercorp/service-base")).catch(() => process.exit(1));
 
-  console.log(`New plugin setup ${ argv.name } of type ${ argv.type }`);
+  console.log(`New plugin setup ${ pluginName } of type ${ argv.type }`);
   console.log(`Enable the plugin in the /sec.config.json file`);
 })();
