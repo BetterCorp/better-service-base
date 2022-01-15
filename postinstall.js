ce596539e4c88bde16b7fa6a7ef7d80116ef82d6
Mitchell R <support@bettercorp.co.za>
2022-01-15 15:18:16 +0200

    Updated post-install script .... fix installer

--- postinstall.js
+++ postinstall.js
@@ -2,5 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const cwd = process.cwd();
 
-if (JSON.parse(fs.readFileSync(path.join(cwd, './package.json')).toString()).name !== '@bettercorp/service-base')
-  require('./lib/install.js');
+if (fs.existsSync(path.join(cwd, './.bsb.local'))) {
+  console.log('BSB Post Install: Ignore');
+} else {
+  console.log('BSB Post Install: Run');
+  const installer = require(path.join(cwd, './lib/install.js')).default;
+  console.log(fs.readdirSync('./build/'))
+  installer(path.join(cwd, '../../../'));
+  console.log('BSB Post Install: Complete');
+}
