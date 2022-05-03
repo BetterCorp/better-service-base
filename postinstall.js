b067e5e48f2a9f0ef92bff1037c09c81548f111d
Mitchell R <support@bettercorp.co.za>
2022-05-03 19:47:12 +0200

    post-install ignored when inside a node_modules dir

--- postinstall.js
+++ postinstall.js
@@ -6,6 +6,10 @@ if (fs.existsSync(path.join(cwd, './.bsb.local'))) {
   console.log('BSB Post Install: Ignore');
 } else {
   console.log('BSB Post Install: Run');
+  if (cwd.indexOf('/node_modules/') > 0) {
+    console.log('BSB Post Install: Bypass - we`re in node_modules dir.')
+    process.exit(0);
+  }
   const installer = require(path.join(cwd, './lib/install.js')).default;
   console.log(fs.readdirSync('./build/'))
   installer(path.join(cwd, '../../../'));
