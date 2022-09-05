109b179d83eca18dd3b5c01f959995f7c3c73fc5
Mitchell R <support@bettercorp.co.za>
2022-09-05 09:42:00 +0200

    Added services for testing to pack

--- src/plugins/service-default2/plugin.ts
+++ src/plugins/service-default2/plugin.ts
@@ -1,32 +1,11 @@
 import { ServicesBase } from "../../service/service";
-import { ServiceCallable } from "../../service/base";
-import { testClient } from "../service-default/plugin";
+import { testClient } from "../service-default1/plugin";
 
-export interface testCallable extends ServiceCallable {
-  abc(a: boolean): Promise<void>;
-}
-
-export interface testEvents extends ServiceCallable {
-  abcd(a: boolean): Promise<void>;
-}
-
-export interface testRetEvents extends ServiceCallable {
-  abcdx(a: boolean): Promise<boolean>;
-}
-
-export class Service
-  extends ServicesBase<testEvents, testRetEvents, testCallable, any>
-  implements testCallable
-{
-  async abc(): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
+export class Service extends ServicesBase {
+  public override initRequiredPlugins: string[] = ["service-default1"];
   async run() {
-    //console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA1')
     let ppp = new testClient(this);
-    //console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA2')
     await ppp.register();
-    //console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA3')
-    ppp.abc(false);
+    ppp.abc();
   }
 }
