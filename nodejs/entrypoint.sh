de5c04e1497990bdc6125d63ddd6958be4903e65
Mitchell R <mitchell.robert@bettercorp.co.za>
2023-01-23 01:55:09 +0200

    Added dialout group for node - serial related

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -7,6 +7,10 @@ if [ "$BSB_CONTAINER" == "true" ]; then
   # node ./node_modules/@bettercorp/service-base/postinstall.js --cwd=$(pwd)
 fi
 
+if [ "$BSB_DIALOUT" == "true" ]; then
+  addgroup node dialout;
+fi
+
 chown -R root:node /home/bsb
 chmod -R 650 /home/bsb
 chown node:node /home/bsb/sec.config.json
