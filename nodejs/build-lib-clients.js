6d1952b397b2d3375179f514c29396352940bf85
Mitchell R <mitchell.robert@bettercorp.co.za>
2023-04-09 16:48:38 +0200

    Updated client build with a new shared dir. Specific index for clients if required

--- nodejs/build-lib-clients.js
+++ nodejs/build-lib-clients.js
@@ -26,22 +26,52 @@ if (clients.length === 0) {
 let distDir = path.join(cwd, "./dist-clients");
 if (fs.existsSync(distDir)) fs.rmSync(distDir, { recursive: true });
 fs.mkdirSync(distDir);
-let indexDTS = path.join(libDir, "./index.d.ts");
-let indexJS = path.join(libDir, "./index.js");
-if (!fs.existsSync(indexDTS)) indexDTS = false;
-if (!fs.existsSync(indexJS)) indexJS = false;
 
+let sharedDir = path.join(libDir, "./shared");
+if (!fs.existsSync(sharedDir)) {
+  sharedDir = false;
+}
+const indexExts = [".js", ".d.ts"];
 for (let client of clients) {
+  let indexFile = path.join(libDir, "./index." + client);
+  let indexIsMain = true;
+  if (!fs.existsSync(indexFile + indexExts[0])) {
+    indexIsMain = false;
+    indexFile = path.join(libDir, "./index");
+  }
+  if (!fs.existsSync(indexFile + indexExts[0])) indexFile = false;
+
   console.log(`Building client: ${client}...`);
   let clientDir = path.join(clientsDir, client);
   let clientDistDir = path.join(distDir, client);
   fs.mkdirSync(clientDistDir);
   let clientsDistLibDir = path.join(clientDistDir, "lib");
   fs.mkdirSync(clientsDistLibDir);
-  if (indexDTS)
-    fs.copyFileSync(indexDTS, path.join(clientsDistLibDir, "index.d.ts"));
-  if (indexJS)
-    fs.copyFileSync(indexJS, path.join(clientsDistLibDir, "index.js"));
+  if (indexFile !== false) {
+    for (let indexExt of indexExts) {
+      let indexFileExt = indexFile + indexExt;
+      if (fs.existsSync(indexFileExt)) {
+        console.log(
+          `Copying index file: ${indexFileExt} -> ${path.join(
+            clientsDistLibDir,
+            "index" + indexExt
+          )}`
+        );
+        fs.copyFileSync(
+          indexFileExt,
+          path.join(clientsDistLibDir, "index" + indexExt)
+        );
+      }
+    }
+  }
+  if (sharedDir !== false) {
+    let clientsDistLibSharedDir = path.join(clientsDistLibDir, "shared");
+    console.log(
+      `Copying shared dir: ${sharedDir} -> ${clientsDistLibSharedDir}`
+    );
+    fs.mkdirSync(clientsDistLibSharedDir);
+    fs.cpSync(sharedDir, clientsDistLibSharedDir, { recursive: true });
+  }
   let clientsDistLibClientsDir = path.join(clientsDistLibDir, "clients");
   fs.mkdirSync(clientsDistLibClientsDir);
   let clientsDistLibClientsClientDir = path.join(
@@ -54,8 +84,18 @@ for (let client of clients) {
   fs.cpSync(pkgJSONPath, dstPkgJSONFile);
   let pkgg = JSON.parse(fs.readFileSync(dstPkgJSONFile, "utf-8").toString());
   pkgg.scripts = {};
-  pkgg.main = `lib/clients/${client}/plugin.js`;
+  pkgg.main =
+    indexIsMain && indexFile !== false
+      ? "lib/index.js"
+      : `lib/clients/${client}/plugin.js`;
+  console.log(`Setting main entrypoint to: ${pkgg.main}`);
   pkgg.files = ["lib/**/*"];
+  if (indexFile !== false) {
+    pkgg.files.push("index.js");
+  }
+  if (sharedDir !== false) {
+    pkgg.files.push("shared/**/*");
+  }
   pkgg.bin = {};
   pkgg.bsb_project = undefined;
   pkgg.name = `@bsb-client/${client}`;
