204225a8dcb67134838d04041ae04e28d8eb30c1
mrinc <github@mrincworld.com>
2023-12-23 00:09:36 +0200

    Updating build permissions setup

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -7,13 +7,13 @@
 #   # node ./node_modules/@bettercorp/service-base/postinstall.js --cwd=$(pwd)
 # fi
 
-chown -R root:node /home/bsb
-chmod -R 650 /home/bsb
+chown -R node:node /home/bsb
+chmod -R 440 /home/bsb
 mkdir /home/bsb/.temp
-chmod -R 660 /home/bsb/.temp
+chmod -R 440 /home/bsb/.temp
 chown node:node /home/bsb/sec-config.yaml
-chown -R root:node /mnt/bsb-plugins
-chmod -R 660 /mnt/bsb-plugins
+chown -R node:node /mnt/bsb-plugins
+chmod -R 440 /mnt/bsb-plugins
 
 #exec gosu node "$@"
 exec gosu node "$@"
