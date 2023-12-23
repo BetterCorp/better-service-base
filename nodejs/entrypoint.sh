a03dba645c54a8d33057095ff7929c00f5e9937c
mrinc <github@mrincworld.com>
2023-12-23 12:14:29 +0200

    Added CMD based debugging. Using CMD instead of an ENV

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -17,5 +17,17 @@ chmod -R 644 /home/bsb/.temp
 chmod -R 444 /mnt/bsb-plugins
 chmod 600 /home/bsb/sec-config.yaml
 
-
-exec gosu node:node node lib/cli.js
+# Check if the first argument is BSBDEBUG for debugging purposes
+if [ "$1" = "BSBDEBUG" ]; then
+  shift
+  echo "WARNING: RUNNING IN DEBUG MODE"
+  echo "IN THIS MODE, ANY COMMAND CAN BE RUN"
+  echo "IT WILL BE RUN AS THE NODE USER"
+  echo "DO NOT USE IN PRODUCTION"
+  echo " - THERE WILL BE A 15s DELAY NOW"
+  sleep 15s
+  echo " - RUNNING YOUR COMMAND [$@]"
+  exec gosu node:node "$@"
+else
+  exec gosu node:node node lib/cli.js
+fi
